537ab6d6ab64d98c3e654591541058325ee7aef5
baby-platom <platon200502@gmail.com>
2023-08-28 19:04:39 +0600

    parallel requests to accrual

--- internal/accrual/update.go
+++ internal/accrual/update.go
@@ -1,32 +1,43 @@
 package accrual
 
 import (
+	"sync"
+
 	"github.com/baby-platom/loyalty-system/internal/config"
 	"github.com/baby-platom/loyalty-system/internal/database"
 )
+
+func updateOrderCopy(orderCopy database.Order, order database.Order, ordersCopy *[]database.Order, changed *bool, wg *sync.WaitGroup) {
+	orderData, err := GetInfoAboutOrder(orderCopy.Number)
+	if err == nil && orderData != (OrderData{}) {
+		orderCopy.Status = OrderStatusByAccrual[orderData.Status]
+		if orderCopy.Status == database.PROCESSED {
+			orderCopy.Accrual = orderData.Accrual
+			*changed = true
+		}
+
+		if order.Status != orderCopy.Status {
+			*changed = true
+		}
+	}
+
+	*ordersCopy = append(*ordersCopy, orderCopy)
+	wg.Done()
+}
 
 func GetOrdersCopyWithUpdatedFields(orders []database.Order) (ordersCopy []database.Order, changed bool) {
 	ordersCopy = make([]database.Order, len(orders))
 	copy(ordersCopy, orders)
 
 	if config.Config.Local {
+		var wg sync.WaitGroup
 		for i, orderCopy := range ordersCopy {
 			if orderCopy.Status == database.NEW || orderCopy.Status == database.PROCESSING {
-				orderData, err := GetInfoAboutOrder(orderCopy.Number)
-				if err == nil && orderData != (OrderData{}) {
-					orderCopy.Status = OrderStatusByAccrual[orderData.Status]
-					if orderCopy.Status == database.PROCESSED {
-						orderCopy.Accrual = orderData.Accrual
-						changed = true
-					}
-
-					if orders[i].Status != orderCopy.Status {
-						changed = true
-					}
-				}
+				wg.Add(1)
+				go updateOrderCopy(orderCopy, orders[i], &ordersCopy, &changed, &wg)
 			}
-			ordersCopy[i] = orderCopy
 		}
+		wg.Wait()
 	}
 	return
 }
